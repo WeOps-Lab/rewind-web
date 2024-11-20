c13e52736e0438ab5937399588a0233d8c5776c3
qiu <qiujiaxp@163.com>
2024-11-20 18:18:58 +0800

    feat: add example menu

--- src/app/example/constants/menu.ts
+++ src/app/example/constants/menu.ts
@@ -1,7 +1,6 @@
 const getMenuItems = (formatMessage: any) => {
   return [
-    { label: formatMessage({ id: 'studio.menu' }), icon: 'jiqiren2', path: '/studio' },
-    { label: formatMessage({ id: 'knowledge.menu' }), icon: 'zhishiku1', path: '/knowledge' },
+    { label: formatMessage({ id: 'studio.menu' }), icon: 'jiqiren2', path: '/example' },
   ];
 };
 
